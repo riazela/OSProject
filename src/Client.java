cd9b148a041a193391ba29b9e26c8838a83bd3b7
Mehrnoosh <mhrnshrf@gmail.com>
2018-10-17 22:24:27 -0400

    Client updated accourding testcase format

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,7 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -8,11 +11,13 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class Client implements RequestCallback{
 	public static ArrayList<Client> allClients = new ArrayList<Client>();
 	public static ArrayList<Request> allRequests = new ArrayList<Request>();
 	private static boolean waitingForNewClients = true;
+	private static String sharedFile="output.txt";
 	
 		
 	public static void waitForClient(int port) {
@@ -67,12 +72,15 @@ public class Client implements RequestCallback{
 	private boolean hasPendingReq;
 	private boolean isGranted;
 	private int currentReqTS;
+	private int currentWriteVal;
+	
 	
 	public Client(Socket socket) throws IOException {
 		this.socket = socket;
 		this.hasPendingReq = false;
 		this.isGranted = false;
 		this.currentReqTS = -1;
+		this.currentWriteVal = 0;
 		
 		socket.setSoTimeout(1000);
 		
@@ -121,29 +129,41 @@ public class Client implements RequestCallback{
 					this.close();
 					return;
 				}
+				if (type == 'w') {
+					try {
+						currentWriteVal = messageParts[2].charAt(0);
+					} catch (Exception e) {
+						System.out.println("Closing the client on port " + this.socket.getPort() + ", no value given for write");
+						outputStream.write("No given value for write! \n");
+						outputStream.flush();
+						// TODO: handle exception
+						this.close();
+						return;
+					}
+				}
 				sendReq(type);
 				this.hasPendingReq = true;
 				outputStream.write("Request sent to Server \n");
 				outputStream.flush();
 				break;
-			case "release":
-				if(!hasPendingReq) {
-					System.out.println("Closing the client on port " + this.socket.getPort() + ", released while has no request");
-					outputStream.write("Has no pending request to release! \n");
-					outputStream.flush();
-					this.close();
-					return;	
-				}
-				else if(!isGranted) {
-					System.out.println("Closing the client on port " + this.socket.getPort() + ", released while not granted");
-					outputStream.write("Has no granted request to release! \n");
-					outputStream.flush();
-					this.close();
-					return;
-				}
-				
-				release();
-				break;
+//			case "release":
+//				if(!hasPendingReq) {
+//					System.out.println("Closing the client on port " + this.socket.getPort() + ", released while has no request");
+//					outputStream.write("Has no pending request to release! \n");
+//					outputStream.flush();
+//					this.close();
+//					return;	
+//				}
+//				else if(!isGranted) {
+//					System.out.println("Closing the client on port " + this.socket.getPort() + ", released while not granted");
+//					outputStream.write("Has no granted request to release! \n");
+//					outputStream.flush();
+//					this.close();
+//					return;
+//				}
+//				
+//				release();
+//				break;
 			case "close":
 				this.close();
 				return;
@@ -191,8 +211,49 @@ public class Client implements RequestCallback{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Request granted, waiting for release... \n");
+		System.out.println("Request granted, Accessing the shared file... \n");
 		
+		if (type == 'w') {
+			try {
+				Scanner in = new Scanner(new FileReader(sharedFile));
+				String last = "";
+				String line = "";
+				while ((line = in.nextLine()) != null) { 
+					last = line;
+				}
+				in.close();
+				String[] lastArr = last.split(":");
+				Thread.sleep(500);
+				FileWriter fw = new FileWriter(sharedFile, true);
+				int lastStamp = Integer.parseInt(lastArr[1]);
+				int lastSum = Integer.parseInt(lastArr[0]);
+				int newStamp = lastStamp + 1;
+				int newSum = lastSum + currentWriteVal;
+				fw.append(Integer.toString(newStamp)+":");
+				Thread.sleep(500);
+				fw.append(Integer.toString(newSum) + "\n");
+				fw.close();
+				
+				try {
+					outputStream.write("Write done successfully! \n");
+					outputStream.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				this.close();
+				return;
+			}
+			
+		}
+		
+		this.release();
+		
+		return;
 	}
 	
 	
